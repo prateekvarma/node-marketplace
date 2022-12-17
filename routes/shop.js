100112d018aefae7a574262c1c7eccc77a17336d
Prateek Varma <prtk.varma@gmail.com>
2022-12-17 16:38:46 -0600

    Injecting dynamic data with Pug

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // console.log("From shop.js", adminData.products); //testing getting data from admin.js
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
