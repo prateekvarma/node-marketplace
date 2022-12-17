73a7c80df4538e3f8382c812b15af02f8ccec49f
Prateek Varma <prtk.varma@gmail.com>
2022-12-17 15:31:51 -0600

    Added two separate exports from admin, and shared in memory data logged from the form

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,19 @@ const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
+//path -> /admin/add-product (GET)
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+// path -> /admin/add-product (POST)
 router.post('/add-product', (req, res, next) =>{
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+//module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log("From shop.js", adminData.products); //testing getting data from admin.js
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
