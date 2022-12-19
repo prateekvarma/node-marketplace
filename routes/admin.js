ec26cb4d184c606566eef39934d93f400a097da7
Prateek Varma <prtk.varma@gmail.com>
2022-12-19 17:23:42 -0600

    Used includes to re-se repetitive parts of html in EJS

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,13 @@ const products = [];
 
 //path -> /admin/add-product (GET)
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product' });
+    res.render('add-product', { 
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
 
 // path -> /admin/add-product (POST)
